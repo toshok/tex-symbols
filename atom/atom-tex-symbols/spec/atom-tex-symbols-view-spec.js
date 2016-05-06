14f8ebf62ed1c904d250424499ef40ada91e693c
Chris Toshok <toshok@gmail.com>
2016-05-05 21:36:26 -0700

    we don't need these files

--- atom/atom-tex-symbols/spec/atom-tex-symbols-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomTexSymbolsView from '../lib/atom-tex-symbols-view';
-
-describe('AtomTexSymbolsView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
